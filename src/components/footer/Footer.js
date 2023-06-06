9d0a90d8aced7047948e08831798dc4301c56f01
mintrug <nguyenngocminhtrung280420@gmail.com>
2023-06-07 01:28:55 +0700

    Update interface

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,86 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faSearch } from "@fortawesome/free-solid-svg-icons"
+import { faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
+  const filterItems = ["All", "Active", "Completed"];
+  const functionItems = [
+    {
+      title: "Add new",
+      className: "plus-icon",
+      icon: faPlus,
+    },
+    {
+      title: "Search",
+      className: "search-icon",
+      icon: faSearch,
+    },
+  ];
+
+  const handleFunction = (e) => {
+    const functionItems = Array.from(document.querySelectorAll("span"))
+    const functionItemClasslist = e.currentTarget.classList
+
+    if (Array.from(functionItemClasslist).includes("function-active")) {
+      document.querySelector(".search-input").classList.add("hidden")
+      functionItemClasslist.remove("function-active")
+    } else {
+      document.querySelector(".search-input").classList.remove("hidden")
+      functionItems.map((functionItem) =>
+        functionItem.classList.remove("function-active")
+      )
+      functionItemClasslist.add("function-active")
+    }
+
+    document.querySelector(".search-input").placeholder = e.currentTarget.title
+    document.querySelector(".search-input").focus()
+  };
+
+  const handleFilter = (e) => {
+    const filterItemElements = Array.from(
+      document.querySelectorAll(".filter-item")
+    );
+
+    filterItemElements.map((filterItemElement) =>
+      filterItemElement.classList.remove("filter-item-active")
+    );
+    e.target.classList.add("filter-item-active");
+  };
+
   return (
     <div className="footer-wrapper">
       <div className="function">
-        <FontAwesomeIcon className="plus-icon function-active" icon={faPlus} />
-        <FontAwesomeIcon className="search-icon" icon={faSearch} />
+        {functionItems.map((functionItem) => (
+          <span
+            title={functionItem.title}
+            key={functionItem.title}
+            className={`${functionItem.className} ${
+              functionItem.className === "plus-icon" ? "function-active" : ""
+            }`}
+            onClick={handleFunction}
+          >
+            <FontAwesomeIcon
+              style={{
+                height: "18px",
+                width: "18px",
+              }}
+              icon={functionItem.icon}
+            />
+          </span>
+        ))}
       </div>
-      {/* <span>|</span> */}
-      <div className="count-content">
-        3 items left
-      </div>
+      <div className="count-content">3 items left</div>
       <div className="filter">
-        <div className="filter-item filter-item-active">All</div>
-        <div className="filter-item">Active</div>
-        <div className="filter-item">Completed</div>
+        {filterItems.map((filterItem) => (
+          <div
+            className={`filter-item ${
+              filterItem === "All" ? "filter-item-active" : ""
+            }`}
+            onClick={handleFilter}
+            key={filterItem}
+          >
+            {filterItem}
+          </div>
+        ))}
       </div>
     </div>
   );
